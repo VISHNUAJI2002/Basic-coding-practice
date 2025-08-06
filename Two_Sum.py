91bc166a5e362c44ac9a2167241be54a720194e7
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-06 23:55:08 +0530

    Create Two_Sum.py

---
+++ Two_Sum.py
@@ -0,0 +1,31 @@
+#printing indices of the two numbers such that they add up to target.
+
+def firstmethod(arr,t):
+    n=len(arr)
+    res=[]
+    for i in range(n-1):
+        for j in range(i,n):
+            if arr[i]+arr[j]==t:
+                res.append([i,j])
+    return res            
+
+def secondmethod(arr,t):
+    mydict={}
+    res=[]
+    i=0
+    for j in arr:
+        diff=t-j
+        if diff in mydict:
+            res.append([mydict[diff],i])
+        mydict[j]=i
+        i+=1
+    return res    
+
+
+
+nums = list(map(int, input().split()))
+target = int(input())
+
+print(firstmethod(nums,target))
+print(secondmethod(nums,target))
+
