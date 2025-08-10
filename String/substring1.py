ae720d1a33de8520db5ff188931ba0b33b76fb6f
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-10 11:17:37 +0530

    Update substring1.py

--- String/substring1.py
+++ String/substring1.py
@@ -1,1 +1,18 @@
+'''finding longest substring without duplicate characters and its length.'''
 
+def solution(s):
+    length=0
+    current=""
+    longest=""
+    for i in s:
+        if i in current:
+            current=current[current.index(i)+1:]
+        current+=i
+        if len(current)>length:
+            length=len(current)
+            longest=current
+    return(longest,length)
+    
+    
+str=input()
+print(solution(str))
