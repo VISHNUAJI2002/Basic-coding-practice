290000491e3375693b04325052121f9697c59a15
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-10 23:41:02 +0530

    Create pal_substr.py
    longest palindromic substring

---
+++ String/pal_substr.py
@@ -0,0 +1,21 @@
+#longest palindromic substring
+
+def palindrome(s):
+    if len(s)<=1:
+        return s
+    pal=""
+    substr=""
+    n=len(s)
+    for i in range(n-1):
+        for j in range(i,n):
+            substr=s[i:j+1]
+            if substr==substr[::-1]:
+                if len(substr)>len(pal):
+                    pal=substr
+        if len(pal)>=n-i:
+            return pal            
+    return pal                
+    
+s=input()
+print(palindrome(s))
+
