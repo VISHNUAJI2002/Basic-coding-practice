0f2779354f5d8e9875e5b79eaff743229f04a1b4
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-15 21:43:26 +0530

    Update uncommonchars.py
    Print uncommon characters between two strings lexicographily

--- String/uncommonchars.py
+++ String/uncommonchars.py
@@ -8,8 +8,11 @@ def uncommonchars1(s1,s2):
     for i in sorted(c):
         res += i
     return res
-
+    
+def uncommonchars2(s1,s2):
+    return ''.join(sorted(set(s1).symmetric_difference(set(s2))))
     
 s1=input()
 s2=input()
 print(uncommonchars1(s1,s2))
+print(uncommonchars2(s1,s2))
