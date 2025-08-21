3fce0882dc5ef4da02c66d815ddd7d125928e1f0
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-22 00:54:13 +0530

    Create minSquares.py
    Given a positive integer N, find the minimum number of perfect square numbers
    (such as 1, 4, 9, 16, ...) whose sum is equal to N.

---
+++ dynamic_programming/minSquares.py
@@ -0,0 +1,19 @@
+'''
+Given a positive integer N, find the minimum number of perfect square numbers
+(such as 1, 4, 9, 16, ...) whose sum is equal to N.
+'''
+
+def minSquares(N):
+    dp = [float('inf')]* (N+1)
+    dp[0] = 0
+    for i in range(1, N+1):
+        j = 1
+        while j*j <= i:
+            dp[i] = min(dp[i], dp[i- j*j]+1)
+            j+=1
+    return dp[N]
+
+
+N = int(input())
+print(minSquares(N))
+
