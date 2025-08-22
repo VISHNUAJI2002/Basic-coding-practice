e60160aa2313958353ac72c485a2fa583f5b7530
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-22 07:16:30 +0530

    Create diamond.py
    diamond patterns (stars)

---
+++ patterns/diamond.py
@@ -0,0 +1,10 @@
+#diamond (stars)
+
+def diamond(n):
+    for i in range(1,n+1):
+        print(" "*(n-i)+"*" * (2*i-1))
+    for i in range(n-1,0,-1):
+        print(" "* (n-i)+"*"*(2*i-1))
+        
+n=int(input())
+diamond(n)
