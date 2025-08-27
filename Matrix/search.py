ffdde9d07ada8d6ec537f87d914f11033f738aac
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-28 00:08:49 +0530

    Create search.py
    Search an element in a matrix

---
+++ Matrix/search.py
@@ -0,0 +1,26 @@
+#Search an element in a matrix
+
+def search(x,m,n,t):
+    l=0
+    r=m*n -1
+    while l<=r:
+        mid=(l+r)//2
+        row=mid//n
+        col=mid%n
+        midval=x[row][col]
+        if midval==t:
+            return True
+        elif midval<t:
+            l=mid+1
+        else:
+            r=mid-1
+    return False        
+        
+    
+    
+rows=int(input())
+cols=int(input())
+matrix=[list(map(int,input().split())) for _ in range(rows)]
+target=int(input())
+
+print(search(matrix,rows,cols,target))
