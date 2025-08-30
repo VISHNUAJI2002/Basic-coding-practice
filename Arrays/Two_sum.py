c64a476efd0ef10c81c5031e8eb15c73dff92293
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-30 23:41:13 +0530

    Update Two_sum.py
    Find indices of the two numbers in an array such that they add up to target.
    Example:
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].

--- Arrays/Two_sum.py
+++ Arrays/Two_sum.py
@@ -1,4 +1,11 @@
-#printing indices of the two numbers such that they add up to target.
+'''
+Find indices of the two numbers in an array such that they add up to target.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
 
 def firstmethod(arr,t):
     n=len(arr)
