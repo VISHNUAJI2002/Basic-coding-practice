4a5679cf4c9ab5a76d14f362b79bbf0c39ad3847
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-30 23:37:07 +0530

    Update 4_sum.py
    Find an array of all the unique quadruplets such that they add upto given target value:
    nums[a] + nums[b] + nums[c] + nums[d] == target
    a, b, c, and d are distinct.
    Example:
    Input: nums = [1,0,-1,0,-2,2], target = 0
    Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]

--- Arrays/4_sum.py
+++ Arrays/4_sum.py
@@ -3,6 +3,9 @@ Find an array of all the unique quadruplets such that they add upto given target
 nums[a] + nums[b] + nums[c] + nums[d] == target
 a, b, c, and d are distinct.
 
+Example:
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 '''
 
 def foursum(nums,t):
