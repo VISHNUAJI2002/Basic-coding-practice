fee17438002236b2b76aeb04eb3e64823f0927db
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-30 22:14:22 +0530

    Update transpose.py
    Transpose of given matrix
    eg:
    4 5 6 
    1 2 3
    [4, 1]
    [5, 2]
    [6, 3]

--- Matrix/transpose.py
+++ Matrix/transpose.py
@@ -1,15 +1,38 @@
 #Transpose of given matrix
 
-def transpose(x):
+def transpose_sqr(x):    #for square matrices
     n=len(x)
     for i in range(n):
         for j in range(i+1,n):
             x[i][j],x[j][i]=x[j][i],x[i][j]
     return x
-    
-    
-rows=int(input("no. of rows:"))
-matrix=[list(map(int,input().split())) for _ in range(rows)]
 
-for i in transpose(matrix):
+def transpose_rect(matrix,m,n):   #for rectangular matrices
+    transposed=[[0]*m for _ in range(n)]
+    for i in range(m):
+        for j in range(n):
+            transposed[j][i]=matrix[i][j]
+    return transposed
+    
+    
+rows=int(input())
+cols=int(input())
+matrix=[list(map(int,input().split()))for _ in range(rows)]
+
+if rows==cols:   # square matrix
+    result=transpose_sqr(matrix)
+else:              # rectangular matrix
+    result=transpose_rect(matrix, rows, cols)
+
+for i in result:
     print(i)
+
+'''
+2
+3
+4 5 6 
+1 2 3
+[4, 1]
+[5, 2]
+[6, 3]
+'''
