37554044c753fe76acd0c2f87a6d4524a9ccf656
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-31 23:50:37 +0530

    Update hotelcost.py
    There are N hotels along the beautiful Adriatic coast. Each hotel has its value in Euros.
    Sroljo has won M Euros on the lottery. Now he wants to buy a sequence of consecutive hotels,
    such that the sum of the values of these consecutive hotels is as great as possible 
    but not greater than M.
    calculate this greatest possible total value.
    Example:
    Input:
    5 12
    2 1 3 4 5
    Output:
    12

--- Arrays/hotelcost.py
+++ Arrays/hotelcost.py
@@ -4,9 +4,13 @@ Sroljo has won M Euros on the lottery. Now he wants to buy a sequence of consecu
 such that the sum of the values of these consecutive hotels is as great as possible 
 but not greater than M.
 calculate this greatest possible total value.
+Example:
+Input:
+5 12
+2 1 3 4 5
+Output:
+12
 '''
-
-
 
 n,m=map(int,input().split())
 h=list(map(int,input().split()))
