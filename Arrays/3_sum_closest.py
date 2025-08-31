95300b02163a7b1e4d1b8c963a1e896db3126c22
Vishnu Aji <vishnuaji2002@gmail.com>
2025-08-31 20:36:14 +0530

    Update 3_sum_closest.py
    Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
    Return the sum of the three integers.
    You may assume that each input would have exactly one solution.
    Example:
    Input: nums = [-1,2,1,-4], target = 1
    Output: 2
    Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).

--- Arrays/3_sum_closest.py
+++ Arrays/3_sum_closest.py
@@ -1,4 +1,12 @@
-'''Find three integers in a list such that the sum is closest to target, and return the sum''' 
+'''
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
+Example:
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+''' 
 
 def three_sum_closest(nums, target):
     n=len(nums)
@@ -19,6 +27,11 @@ def three_sum_closest(nums, target):
                 return current_sum
                 
     return closest_sum
+
+
+nums=list(map(int,input().split()))
+target=int(input())
+print(three_sum_closest(nums,target))
 
 
 nums=list(map(int,input().split()))
