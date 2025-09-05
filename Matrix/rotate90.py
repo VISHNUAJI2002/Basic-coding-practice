fdbd48cf2a7c231e36c6387b31cc143f58e69f4e
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-05 19:13:12 +0530

    Update rotate90.py
    You are given an n x n 2D matrix representing an image,
    rotate the image by 90 degrees (clockwise).
    You have to rotate the image in-place,
    which means you have to modify the input 2D matrix directly. 
    DO NOT allocate another 2D matrix and do the rotation.
    
    Example:
    Input:
    Enter number of rows: 3
    1 2 3
    4 5 6
    7 8 9
    Output:
    [7, 4, 1]
    [8, 5, 2]
    [9, 6, 3]

--- Matrix/rotate90.py
+++ Matrix/rotate90.py
@@ -4,6 +4,17 @@ rotate the image by 90 degrees (clockwise).
 You have to rotate the image in-place,
 which means you have to modify the input 2D matrix directly. 
 DO NOT allocate another 2D matrix and do the rotation.
+
+Example:
+Input:
+Enter number of rows: 3
+1 2 3
+4 5 6
+7 8 9
+Output:
+[7, 4, 1]
+[8, 5, 2]
+[9, 6, 3]
 '''
 
 def rotate(x):
