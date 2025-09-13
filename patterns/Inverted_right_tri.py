7547b0148b7097ed913486cb21883ca230dbd6a9
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-13 23:43:36 +0530

    Update Inverted_right_tri.py
    Inverted right-angled triangle (stars)
    
    Example:
    Input:
    5
    Output:
    *****
    ****
    ***
    **
    *

--- patterns/Inverted_right_tri.py
+++ patterns/Inverted_right_tri.py
@@ -1,5 +1,17 @@
-#Inverted right-angled triangle (stars)
+'''
+Inverted right-angled triangle (stars)
 
+Example:
+Input:
+5
+Output:
+*****
+****
+***
+**
+*
+
+'''
 def inverted_right_tri(n):
     for i in range(n,0,-1):
         print("*" * i)
