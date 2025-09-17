2fe8b6816a1609c5aaba6652bbe2cdcfd670cc31
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-17 21:57:20 +0530

    Update and rename patterns/pascal's_triangle.py to patterns/numbers/pascal's_triangle.py
    Pascal’s Triangle
    
    Example:
    Input:
    5
    Output:
         1 
        1 1 
       1 2 1 
      1 3 3 1 
     1 4 6 4 1

--- patterns/numbers/pascal's_triangle.py
+++ patterns/numbers/pascal's_triangle.py
@@ -1,5 +1,17 @@
-#Pascal’s Triangle
+'''
+Pascal’s Triangle
 
+Example:
+Input:
+5
+Output:
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+'''
+ 
 def pascal(n):
     for i in range(n):
         print(" " * (n-i), end="")
