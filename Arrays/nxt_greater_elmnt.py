864d9acaaac41fd4ae9cd20771d2ce103dbbcc9a
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-18 22:48:20 +0530

    Update nxt_greater_elmnt.py
    find the next greater element for each element of the array in order of their appearance in the array.
    Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
    If there does not exist next greater of current element, then next greater element for current element is -1. For example, 
    next greater of the last element is always -1.
    
    Example:
    Input: 5 3 1 2 4 6 0 8 9
    Output: [6, 4, 2, 4, 6, 8, 8, 9, -1]

--- Arrays/nxt_greater_elmnt.py
+++ Arrays/nxt_greater_elmnt.py
@@ -3,7 +3,12 @@ find the next greater element for each element of the array in order of their ap
 Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
 If there does not exist next greater of current element, then next greater element for current element is -1. For example, 
 next greater of the last element is always -1.
+
+Example:
+Input: 5 3 1 2 4 6 0 8 9
+Output: [6, 4, 2, 4, 6, 8, 8, 9, -1]
 '''
+
 def next_greater_elements(arr):
     n = len(arr)
     res = [-1] * n  #default result
