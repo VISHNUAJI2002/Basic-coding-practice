1782723d9c2de26a5757721b92f7977ae7a5b9e7
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-21 10:48:14 +0530

    Update solution: longest_substr_without_rep_char.py
    Given a string s, find the length of the longest substring without duplicate characters.
    
    Example :
    Input: s = "abcabcbb"
    Output: 3
    Explanation: The answer is "abc", with the length of 3.

--- String/longest_substr_without_rep_char.py
+++ String/longest_substr_without_rep_char.py
@@ -1,17 +1,22 @@
-#finding longest substring without duplicate characters and its length.
+'''
+Given a string s, find the length of the longest substring without duplicate characters.
+
+Example :
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+'''
 
 def solution(s):
     length=0
     current=""
-    longest=""
     for i in s:
         if i in current:
             current=current[current.index(i)+1:]
         current+=i
         if len(current)>length:
             length=len(current)
-            longest=current
-    return(longest,length)
+    return length
     
     
 str=input()
