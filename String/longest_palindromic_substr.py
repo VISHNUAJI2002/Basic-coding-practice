41ff2a7375c56245af3cc4d7781ccc8d25fc225a
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-21 14:22:45 +0530

    Update and rename pal_substr.py to longest_palindromic_substr.py
    Given a string s, return the longest palindromic substring in s.
    Example :
    Input: s = "babad"
    Output: "bab"
    Explanation: "aba" is also a valid answer.

--- String/longest_palindromic_substr.py
+++ String/longest_palindromic_substr.py
@@ -1,4 +1,10 @@
-#longest palindromic substring
+'''
+Given a string s, return the longest palindromic substring in s.
+Example :
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+'''
 
 def palindrome(s):
     if len(s)<=1:
