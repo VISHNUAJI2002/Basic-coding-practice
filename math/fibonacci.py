ec548df971af6dda8aa43beecbdf103bf0a8f025
Vishnu Aji <vishnuaji2002@gmail.com>
2025-09-28 22:10:54 +0530

    Create fibonacci.py
    Fibonacci series
    Example:
    Input: 10
    Output: 0 1 1 2 3 5 8 13 21 34

---
+++ math/fibonacci.py
@@ -0,0 +1,16 @@
+'''
+Fibonacci series
+Example:
+Input:10
+Output:0 1 1 2 3 5 8 13 21 34 
+'''
+
+def fibonacci(n):
+    a=0
+    b=1
+    for _ in range(n):
+        print(a,end=" ")
+        a,b=b,a+b
+        
+n=int(input())
+fibonacci(n)
