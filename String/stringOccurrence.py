4ce37e192ce8aa33fda8222393ff5bd0afaa7f61
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-02 22:28:38 +0530

    Update and rename StringOccurrence.py to stringOccurrence.py
    For two strings "main" and "sub", return the index of the 
    first occurrence of sub in main, or -1 if sub is not part of main.
    
    Example:
    Input:
    main = "hello world"
    sub = "or"
    Output: 7
    Explanation: "or" starts at index 2 in "hello world".

---
+++ String/stringOccurrence.py
@@ -0,0 +1,27 @@
+'''
+For two strings "main" and "sub", return the index of the 
+first occurrence of sub in main, or -1 if sub is not part of main.
+
+Example:
+Input:
+main = "hello world"
+sub = "or"
+Output: 7
+Explanation: "or" starts at index 2 in "hello world".
+'''
+
+def strstr(s1, s2):
+    n = len(s1)
+    m = len(s2)
+    if n < m:
+        return -1
+    for i in range(n + 1 - m):
+        if s1[i] == s2[0]:
+            if s1[i:i+m] == s2:
+                return i
+    return -1
+
+
+main = input()
+sub = input()
+print(strstr(main, sub))
