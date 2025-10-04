04872cfecdfb2c95653c87a4c7e0821cd24abc65
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-04 22:17:22 +0530

    Update longestValidParentheses.py
    Longest Valid Parentheses.
    Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
    
    Example 1:
    Input: s = "(()"
    Output: 2
    Explanation: The longest valid parentheses substring is "()".
    
    Example 2:
    Input: s = ")()())"
    Output: 4
    Explanation: The longest valid parentheses substring is "()()".

--- String/longestValidParentheses.py
+++ String/longestValidParentheses.py
@@ -1,8 +1,16 @@
 '''
-Given a string s containing just the characters '(' and ')', 
-return the length of the longest valid (well-formed) parentheses substring.
-A valid parentheses substring is one where every opening parenthesis '(' 
-has a matching closing parenthesis ')' in the correct order.
+Longest Valid Parentheses.
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+
+Example 2:
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
 '''
 
 def longestValidParentheses(s):
