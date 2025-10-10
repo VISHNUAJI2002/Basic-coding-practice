b767a8a56cdf6a5cb9a02d38684fa56f6bb69615
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-10 18:57:37 +0530

    Update automorphic_number.py
    Check if a number is an Automorphic Number.
    An Automorphic number is a number whose square ends with the same digits as the number itself.
    
    Example:
    Input: 76
    Output: True
    Explanation: 76² = 5776 → ends with 76

--- math/automorphic_number.py
+++ math/automorphic_number.py
@@ -8,7 +8,7 @@ Output: True
 Explanation: 76² = 5776 → ends with 76
 '''
 
-#(without using string operations)
+#without using string operations
 def is_automorphic(n):
     sq=n*n
     temp=abs(n)
@@ -18,7 +18,16 @@ def is_automorphic(n):
         temp//=10
 
     return sq%(10**digits)==abs(n)        
+
+
+#using string operations
+def automorphic(n):
+    sq = n * n
+    return str(sq).endswith(str(abs(n)))
     
 
 n = int(input())
 print(is_automorphic(n))
+print(automorphic(n))
+
+
