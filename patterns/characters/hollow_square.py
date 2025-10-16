81401ccd5afc558d09296df17aa4d6a55bf0fa35
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-17 05:24:15 +0530

    Create hollow_square.py
    Hollow square star pattern.
    
    Example:
    Input:
    5
    Output:
    *****
    *   *
    *   *
    *   *
    *****

---
+++ patterns/characters/hollow_square.py
@@ -0,0 +1,23 @@
+'''
+Hollow square star pattern.
+
+Example:
+Input:
+5
+Output:
+*****
+*   *
+*   *
+*   *
+*****
+'''
+
+def hollow_square(n):
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            print('*' * n)
+        else:
+            print('*' + ' ' * (n - 2) + '*')
+
+n = int(input())
+hollow_square(n)
