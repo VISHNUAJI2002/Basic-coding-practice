0203340d03e6cad07fe66cfd6f45614ec8db907d
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-23 15:39:27 +0530

    Update continous_number_square.py
    Continuous number square pattern.
    
    Example:
    Input: 
    4
    Output:
      1   2   3   4 
      5   6   7   8 
      9  10  11  12 
     13  14  15  16

--- patterns/numbers/continous_number_square.py
+++ patterns/numbers/continous_number_square.py
@@ -5,17 +5,17 @@ Example:
 Input: 
 4
 Output:
-1 2 3 4
-5 6 7 8
-9 10 11 12
-13 14 15 16
+  1   2   3   4 
+  5   6   7   8 
+  9  10  11  12 
+ 13  14  15  16 
 '''
 
 def continuous_number_square(n):
     num=1
     for i in range(n):
         for j in range(n):
-            print(num,end=" ")
+            print(f"{num:3}",end=" ")
             num+=1
         print()
 
